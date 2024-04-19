62540235981dca56460eb7e6022206730a2b6db7
siddharthbadal <siddharthabadal.2015@gmail.com>
2024-04-19 18:25:20 +0530

    job details page

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const sortingBtnRelevantEl = document.querySelector(".sorting__button--relevant"
 const sortingBtnRecentEl = document.querySelector(".sorting__button--recent");
 const spinnerSearchEl = document.querySelector(".spinner--search");
 const spinnerJobDetailsEl = document.querySelector(".spinner--job-details");
+const jobsAvilableEL= document.querySelector('.jobs-available');
 
 
 //  search component 
@@ -54,7 +55,7 @@ const submitHandler =(e)=>{
 
 // https://bytegrad.com/course-assets/js/2/api/jobs
     // fetch search results 
-    fetch(`https://techsidh1.github.io/feedback-data-api/jobs.json??search=${searchText}`)
+    fetch(`https://techsidh1.github.io/feedback-data-api/jobs.json?search=${searchText}`)
         .then(response => {
             if (!response.ok){
                 console.log("Something is not okay. try again.")
@@ -73,6 +74,9 @@ const submitHandler =(e)=>{
 
             //  render number of jobs
             numberEl.textContent= jobItems.length;
+            
+            // page footer
+            jobsAvilableEL.textContent=`Total ${jobItems.length} jobs`;
             
             jobItems.slice(0,7).forEach((jobItem)=>{                         
                 
@@ -111,3 +115,134 @@ const submitHandler =(e)=>{
 searchFormEl.addEventListener('submit', submitHandler)
 
 
+// Job list component 
+const clickHandler = (event)=>{
+    //  prevent default behaviour 
+    event.preventDefault();
+
+    // check for the correct clicked job item
+    const jobItemEl = event.target.closest('.job-item')
+    
+    // remove the active class from previously active job item
+    // first way of doing
+    // const activeJobItemEl = document.querySelector('.job-item--active')
+    // if (activeJobItemEl){
+    //     activeJobItemEl.classList.remove('job-item--active')
+    // }
+
+    // second way of doing
+    // document.querySelector('.job-item--active') && document.querySelector('.job-item--active').classList.remove('.job-item--active')
+
+    // third way of doing - option chaining
+    document.querySelector('.job-item--active')?.classList.remove('job-item--active')
+
+
+    // add active class on list item
+    jobItemEl.classList.add('job-item--active')
+
+
+
+    // job details ection emptry the text and load pinner
+    jobDetailsContentEl.innerHTML = '';
+    // render spiiner
+    spinnerJobDetailsEl.classList.add('spinner--visible');
+
+
+    // get the id of clicked job item
+    const id = jobItemEl.children[0].getAttribute(['href']);
+
+    // fetch job item data
+    fetch(`https://bytegrad.com/course-assets/js/2/api/jobs/${id}`)
+    .then(response =>{
+        if(!response.ok){
+            console.log("Something went wrong!")
+            return;
+        }
+       return response.json();
+    })
+    .then(data => {
+        // extract job details 
+        const { jobItem } = data;
+        
+        //  remove spinner
+        spinnerJobDetailsEl.classList.remove('spinner--visible');
+
+        const jobDetailsHTML = `
+        <img src="${jobItem.coverImgURL}" alt="#" class="job-details__cover-img">
+
+        <a class="apply-btn" href="${jobItem.companyURL}" target="_blank">Apply <i class="fa-solid fa-square-arrow-up-right apply-btn__icon"></i></a>
+        
+        <section class="job-info">
+            <div class="job-info__left">
+                <div class="job-info__badge">
+                ${jobItem.badgeLetters}</div>
+                <div class="job-info__below-badge">
+                    <time class="job-info__time">${jobItem.daysAgo}d</time>
+                    <button class="job-info__bookmark-btn">
+                        <i class="fa-solid fa-bookmark job-info__bookmark-icon"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="job-info__right">
+                <h2 class="second-heading">${jobItem.title}</h2>
+                <p class="job-info__company">${jobItem.company}</p>
+                <p class="job-info__description">${jobItem.description}</p>
+                <div class="job-info__extras">
+                    <p class="job-info__extra"><i class="fa-solid fa-clock job-info__extra-icon"></i> ${jobItem.duration}</p>
+                    <p class="job-info__extra"><i class="fa-solid fa-money-bill job-info__extra-icon"></i> ${jobItem.salary}</p>
+                    <p class="job-info__extra"><i class="fa-solid fa-location-dot job-info__extra-icon"></i> ${jobItem.location}</p>
+                </div>
+            </div>
+        </section>
+        
+        <div class="job-details__other">
+            <section class="qualifications">
+                <div class="qualifications__left">
+                    <h4 class="fourth-heading">Qualifications</h4>
+                    <p class="qualifications__sub-text">Other qualifications may apply</p>
+                </div>
+                <ul class="qualifications__list">
+                // qualifications is a list so need to format
+                    ${jobItem.qualifications.map(qualification => 
+                        `<li class='qualifications__item'>${qualification}</li>`).join('')}
+                        
+                </ul>
+            </section>
+        
+            <section class="reviews">
+                <div class="reviews__left">
+                    <h4 class="fourth-heading">Company reviews</h4>
+                    <p class="reviews__sub-text">Recent things people are saying</p>
+                </div>
+                <ul class="reviews__list">
+                ${jobItem.reviews.map(review =>`<li class='reviews__item'>${review}</li>`).join('')}
+                    
+                </ul>
+            </section>
+        </div>
+        
+        <footer class="job-details__footer">
+            <p class="job-details__footer-text">If possible, please reference that you found the job on <span class="u-bold">rmtDev</span>, we would really appreciate it!</p>
+        </footer>
+        
+        `;
+
+        // insert in the right element
+        jobDetailsContentEl.innerHTML = jobDetailsHTML;
+
+
+
+
+
+    })
+    .catch(error => console.log(error));
+
+
+
+}
+
+
+
+
+
+jobListSearchEl.addEventListener('click', clickHandler)
